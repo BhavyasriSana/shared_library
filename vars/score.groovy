8fa88906b91e64145851791f748a92c7dfd35316
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-06 18:32:54 +0530

    Update score.groovy

--- vars/score.groovy
+++ vars/score.groovy
@@ -26,12 +26,12 @@ create(json){
   score+=10;
   LIST.add(["metric":"Team Failure Builds","Value":score,"Tool":"JENKINS"])
   }
-  def indCount=jsonObj.JENKINS.individualsuccess.size()
-	print(indCount)
+  score=0;
   for(j=0;j<mailcount;j++)
    {
     def email=json.config.emails.email[j].mail
 	   print(email)
+	   def indCount=jsonObj.JENKINS.individualsuccess.size()
      for(k=0;k<indCount;k++){
        def mail=jsonObj.JENKINS.individualsuccess[k].email
 	     print(mail)
@@ -40,6 +40,21 @@ create(json){
          if(countS>1){
            score+=10;
            LIST2.add(["email":mail,"metric":"Successfull Builds","Value":score,"Tool":"JENKINS"])
+		 score=0;
+         }
+       }
+     }
+	   score=10;
+	   def indCount=jsonObj.JENKINS.individualfailure.size()
+     for(k=0;k<indCount;k++){
+       def mail=jsonObj.JENKINS.individualfailure[k].email
+	     print(mail)
+       if(email.equals(mail)){
+         def countS=jsonObj.JENKINS.individualfailure[k].Failure_cnt
+         if(countS<5){
+           score+=10;
+           LIST2.add(["email":mail,"metric":"Failure Builds","Value":score,"Tool":"JENKINS"])
+		 score=0;
          }
        }
      }
