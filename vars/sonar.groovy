1d2ed10aad99112036f588c7358f96cee6d3f884
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-09 19:22:51 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -21,9 +21,9 @@ def jsonObj = readJSON text: jsonString
 String a = jsonObj.code_quality.projects.project.project_key
 String ProjectName=a.replaceAll("\\[", "").replaceAll("\\]","");
   
-//withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
+withCredentials([usernamePassword(credentialsId: 'sonar_cred', passwordVariable: 'password', usernameVariable: 'username')]){
   sh "curl -u admin:admin -X GET 'http://ec2-3-133-107-212.us-east-2.compute.amazonaws.com:9000/api/measures/component?component=${ProjectName}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
   echo 'metrics collected'
-//}
+}
   create()
 }
