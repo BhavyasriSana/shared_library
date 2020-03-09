ac33eed42033c34d05e324d55ad685f8bff94948
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-09 20:17:50 +0530

    Update UserDetails.groovy

--- vars/UserDetails.groovy
+++ vars/UserDetails.groovy
@@ -10,9 +10,10 @@ def jsonString = JSON
 def jsonObj = readJSON text: jsonString
 def mailcount = jsonObj.riglet_info.auth_users.size()
 	def pro = jsonObj.ci.jobs.job.job_name
+	String ProjectName=pro.replaceAll("\\[", "").replaceAll("\\]","");
 	print(mailcount)
 //sh "curl -X GET -g http://52.14.229.175:8080/job/${JOB_NAME}/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u suneel:11035ac86f58bc32d03d8e873b7cc063a3 -o username.json"
-	sh "curl -X GET -g http://18.224.172.87:8080/job/Avengers/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u BhavyasriSana:1185dbd72329ba8cbfc4f9179ed0d48c7a -o username.json"
+	sh "curl -X GET -g http://18.224.172.87:8080/job/${ProjectName}/api/json?tree=builds[id,result,changeSets[items[authorEmail]]] -u BhavyasriSana:1185dbd72329ba8cbfc4f9179ed0d48c7a -o username.json"
 	def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/username.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
