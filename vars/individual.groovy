57f7588c85bb0fe3511592c286b6cd02a393f16d
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-11 19:19:20 +0530

    Update individual.groovy

--- vars/individual.groovy
+++ vars/individual.groovy
@@ -1,7 +1,7 @@
 import groovy.json.*
 	
 
-def call(jsondata,github,jenkins,sonar){
+def call(jsondata,jenkins){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.riglet_info.auth_users.size()
@@ -19,32 +19,49 @@ List<String> jsonStringa= new ArrayList<String>();
     int score=0
     String name="  "
 	  String metric=" "
-if(jsonStringa[i].contains("GITHUB"))
+/*if(jsonStringa[i].contains("GITHUB"))
     {
     name="github"
     def jsonObj1 = readJSON text: jsonStringa[i]
   def count =jsonObj1.GITHUB.commits_count
   LIST.add(["toolName":name,"metricName":"commits","value":count])
-   }
+   }*/
+	  
    if(jsonStringa[i].contains("JENKINS"))
     {
       name="jenkins"
       def jsonObjb = readJSON text: jsonStringa[i]
-	    def indCount=jsonObjb.JENKINS.individualsuccess.size()
+	    def indCountS=jsonObjb.JENKINS.individualsuccess.size()
+	    for(k=0;k<indCountS;k++){
+       def emailS=jsonObjb.JENKINS.individualsuccess[k].email
+	     print(emailS)
+       if(mail.equals(emailS)){
+         def countS=jsonObjb.JENKINS.individualsuccess[k].Success_cnt
+	    LIST.add(["metricName":"successful_builds","toolName":name,"value":countS])
+       }
+	    }
+	    
+	    def indCountF=jsonObjb.JENKINS.individualfailure.size()
+	    for(k=0;k<indCountF;k++){
+       def emailF=jsonObjb.JENKINS.individualfailure[k].email
+	     print(emailF)
+       if(mail.equals(emailF)){
+         def countF=jsonObjb.JENKINS.individualfailure[k].Failure_cnt
+	    LIST.add(["metricName":"failure_builds","toolName":name,"value":countF])
+       }
+	    }
+	    
+	    def indCount=jsonObjb.JENKINS.individualbuilds.size()
 	    for(k=0;k<indCount;k++){
-       def email=jsonObjb.JENKINS.individualsuccess[k].email
-	     print(email)
-       if(mail.equals(email)){
-         def countS=jsonObj.JENKINS.individualsuccess[k].Success_cnt
-	   // print jsonObjb
-      def total=jsonObjb.JENKINS.teambuild_cnt
-  def scnt =jsonObjb.JENKINS.teamsuccessbuild_cnt
-	    def fcnt=jsonObjb.JENKINS.teamfailurebuild_cnt
-	    LIST.add(["toolName":name,"metricName":"total_builds","value":countS])
-	    LIST.add(["toolName":name,"metricName":"successful_builds","value":scnt])
-	    LIST.add(["toolName":name,"metricName":"failure_builds","value":fcnt])
-      }
-      if(jsonStringa[i].contains("Sonar"))
+       def emailT=jsonObjb.JENKINS.individualbuilds[k].email
+	     print(emailT)
+       if(mail.equals(emailT)){
+         def countT=jsonObjb.JENKINS.individualbuilds[k].Failure_cnt
+	    LIST.add(["metricName":"failure_builds","toolName":name,"value":countT])
+       }
+	    }
+    }
+      /*if(jsonStringa[i].contains("Sonar"))
     {
 	    name="sonar"
 	    def jsonObjc = readJSON text: jsonStringa[i]
@@ -56,11 +73,12 @@ if(jsonStringa[i].contains("GITHUB"))
     double data = Double.parseDouble(d); 
        LIST.add(["toolName":name,"metricName":sonar_metric,"value":data])
 	    }
-    }
-     }
+    }*/
+     
 def jsonBuilder = new groovy.json.JsonBuilder()
 
 jsonBuilder(
+"teamMemberName":mail,
  "teamName":team,
   "metrics" : LIST
   
@@ -69,5 +87,6 @@ jsonBuilder(
   File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
   file.write(jsonBuilder.toPrettyString())	
 	//def metrics1 = jsonBuilder.toString()
+}
 }
 }
