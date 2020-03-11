73f20fdef0647757b90aa12d5a1e6cdd0cd89bc6
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-11 13:24:28 +0530

    Update Teamscore.groovy

--- vars/Teamscore.groovy
+++ vars/Teamscore.groovy
@@ -59,7 +59,7 @@ jsonBuilder(
   File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
 file.write(jsonBuilder.toPrettyString())	
 }
-sh 'curl --location --request POST 'http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/teams/add' \
+""" curl --location --request POST 'http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/teams/add' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "teamName": "Avengers",
@@ -120,4 +120,4 @@ sh 'curl --location --request POST 'http://ec2-13-232-248-254.ap-south-1.compute
             "value": 0.0
         }
     ]
-}''
+}'"""
