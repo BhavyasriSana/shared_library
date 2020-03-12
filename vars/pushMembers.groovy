58ef6b334067f555427a954d79d0ba902e7d1a99
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-12 16:22:59 +0530

    Update pushMembers.groovy

--- vars/pushMembers.groovy
+++ vars/pushMembers.groovy
@@ -1,5 +1,5 @@
-def call()
+def call(IP)
 {
-	sh "curl -X POST --header 'Content-Type: application/json' --data @/var/lib/jenkins/workspace/${JOB_NAME}/game.json  http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/members/add"
+	sh "curl -X POST --header 'Content-Type: application/json' --data @/var/lib/jenkins/workspace/${JOB_NAME}/game.json  ${IP}/api/metrics/members/add"
   echo "Sending data to Gamification API"
 }
