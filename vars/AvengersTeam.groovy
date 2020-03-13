63e2fec4227b16fed0d595807078691a734c9737
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-13 13:27:30 +0530

    Update AvengersTeam.groovy

--- vars/AvengersTeam.groovy
+++ vars/AvengersTeam.groovy
@@ -59,7 +59,7 @@ jsonBuilder(
   File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
   file.write(jsonBuilder.toPrettyString())	
 	def metrics1 = jsonBuilder.toPrettyString()
-sh "curl -X POST --header 'Content-Type: application/json' --data ${metrics1}  http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/teams/add"
+sh "curl -X POST --header 'Content-Type: application/json' --data "$(metrics1)"  http://ec2-13-232-248-254.ap-south-1.compute.amazonaws.com:3000/api/metrics/teams/add"
 }
 
 
