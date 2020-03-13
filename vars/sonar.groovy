2bb8f0bec2e0f3c583f5ea0c770de934e89d309d
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-13 17:26:11 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -2,9 +2,7 @@ import groovy.json.*
 
 @NonCPS
 create(){
-  def jsonSlurper = new JsonSlurper()
-  def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json"),"UTF-8"))
-  def resultJson = jsonSlurper.parse(reader)
+def resultJson = readJSON file: 'metrics.json'
   def jsonBuilder = new groovy.json.JsonBuilder()
   jsonBuilder.Sonar(
   "Metrics" : resultJson
