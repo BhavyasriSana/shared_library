4f3763f3a63853761db9ed40eda14c060d3bf1bb
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-13 17:31:58 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,5 +1,5 @@
 import groovy.json.*
-
+/*@NonCPS
 create(){
 def resultJson = readJSON file: 'metrics.json'
   def jsonBuilder = new groovy.json.JsonBuilder()
@@ -9,7 +9,7 @@ def resultJson = readJSON file: 'metrics.json'
   File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json")
   file.write(jsonBuilder.toPrettyString())
   return jsonBuilder
-}
+}*/
 
 def call(jsondata,rig){
 def jsonString = jsondata
@@ -25,6 +25,14 @@ String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
 
   sh "curl -u ${user}:${pass} -X GET '${IP}/api/measures/component?component=${ProjectKey}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
   echo 'metrics collected'
+	def resultJson = readJSON file: 'metrics.json'
+  def jsonBuilder = new groovy.json.JsonBuilder()
+  jsonBuilder.Sonar(
+  "Metrics" : resultJson
+  )
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/metric.json")
+  file.write(jsonBuilder.toPrettyString())
+  return jsonBuilder
 
-  create()
+ // create()
 }
