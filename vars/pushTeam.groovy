6198ca4902ed8de1536d5e1530583112e2803c81
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-15 18:54:16 +0530

    Update pushTeam.groovy

--- vars/pushTeam.groovy
+++ vars/pushTeam.groovy
@@ -1,5 +1,7 @@
-def call(IP)
-{
-	sh "curl -X POST --header 'Content-Type: application/json' --data @/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json  ${IP}/api/metrics/teams/add"
+def call(data,IP){
+def jsonString = data
+def jsonObj = readJSON text: jsonString
+	
+	sh "curl -X POST --header 'Content-Type: application/json' --data ${jsonObj} ${IP}/api/metrics/teams/add"
   echo "Sending data to Gamification API"
 }
