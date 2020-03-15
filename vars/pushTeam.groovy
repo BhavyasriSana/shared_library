f890d8c68bdadeee2d914d5b0ae117c09b2f2207
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-15 18:57:50 +0530

    Update pushTeam.groovy

--- vars/pushTeam.groovy
+++ vars/pushTeam.groovy
@@ -2,6 +2,6 @@ def call(data,IP){
 def jsonString = data
 def jsonObj = readJSON text: jsonString
 	
-	sh "curl -X POST --header 'Content-Type: application/json' --data "${jsonObj}" ${IP}/api/metrics/teams/add"
+	sh "curl -X POST --header 'Content-Type: application/json' --data ${data} ${IP}/api/metrics/teams/add"
   echo "Sending data to Gamification API"
 }
