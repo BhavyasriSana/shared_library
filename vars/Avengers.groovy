e0f7ef9deb6786b2e6a1cab1c4a1f3a63355ca68
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-16 18:27:24 +0530

    Update Avengers.groovy

--- vars/Avengers.groovy
+++ vars/Avengers.groovy
@@ -58,6 +58,74 @@ jsonBuilder(
 	return jsonBuilder
   
   File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/Teamscore.json")
-  file.write(jsonBuilder.toPrettyString())	
+  file.write(jsonBuilder.toPrettyString())
+	
+List<String> JSON = new ArrayList<String>();
+  List<String> LIST2 = new ArrayList<String>();
+  List<String> JSON1 = new ArrayList<String>();
+
+	 for(j=0;j<ecount;j++)
+   {
+	 def email=jsonObj.riglet_info.auth_users[j]
+	   int score=0
+    int reward=0
+    String name="  "
+	 for(i=0;i<jsonStringa.size();i++)
+  { 
+   
+	  
+  if(jsonStringa[i].contains("JENKINS"))
+    {
+ 
+	   
+	    
+     name="jenkins"
+	   def jsonObjj = readJSON text: jsonStringa[i]
+
+  //println(jsonObja)
+  def scnt =jsonObjj.JENKINS.individualsuccess[j].Success_cnt
+  def fcnt =jsonObjj.JENKINS.individualfailure[j].Failure_cnt
+  def tcnt =jsonObjj.JENKINS.individualbuilds[j].Total_cnt
+ def email1=jsonObjj.JENKINS.individualsuccess[j].email
+      
+ if(email==email1)
+  {
+	   LIST2.add(["toolName":name,"metricName":"success_builds","value":scnt])
+	    LIST2.add(["toolName":name,"metricName":"failure_builds","value":fcnt])
+	  LIST2.add(["toolName":name,"metricName":"total_builds","value":tcnt])
+	  
+  }
+   }
+	   
+	  if(jsonStringa[i].contains("GITHUB"))
+      {
+        name="gitlab"
+        def jsonObjg= readJSON text: jsonStringa[i]
+  //println(jsonObj)
+  def cnt =jsonObjg.GITHUB.individual_commit_Details[j].User_Commits_count
+	       def email1=jsonObjg.GITHUB.individual_commit_Details[j].User_email
+	       if(email==email1)
+  {
+    LIST2.add(["toolName":name,"metricName":"commits","value":cnt])
+  }
+      }
+    }
+	   JSON1[j]=LIST2.clone()
+	   
+   JSON.add(["teamMemberName":email,"teamName":team,"metrics":JSON1[j]])
+    LIST2.clear()
+	//reward=0    
+	   
+    }
+	
+     def jsonBuilder = new groovy.json.JsonBuilder()
+
+jsonBuilder(
+   JSON
+  
+) 
+  
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/game.json")
+file.write(jsonBuilder.toPrettyString())
 	
 }
