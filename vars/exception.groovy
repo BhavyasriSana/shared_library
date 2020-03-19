68054362d7a9699d5a92032d2f5767ce4f0f7313
BhavyasriSana <55596685+BhavyasriSana@users.noreply.github.com>
2020-03-19 11:40:57 +0530

    Create exception.groovy

---
+++ vars/exception.groovy
@@ -0,0 +1,27 @@
+def call(jsondata,rig){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+
+String a = jsonObj.code_quality.projects.project.project_key
+String ProjectKey=a.replaceAll("\\[", "").replaceAll("\\]","");
+  
+  def jsonObja = readJSON text: rig
+	def IP=jsonObja.url
+	def user=jsonObja.userName
+	def pass=jsonObja.password
+  def response = sh(script: """curl -u ${user}:${pass} -X GET '${IP}/api/measures/component?component=${ProjectKey}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json""", returnStdout: true)
+  println(response) 
+  //sh "curl -u ${user}:${pass} -X GET '${IP}/api/measures/component?component=${ProjectKey}&metricKeys=coverage,vulnerabilities,bugs,violations,complexity,tests,duplicated_lines,sqale_index' -o metrics.json"
+  echo 'metrics collected'
+	
+	def resultJson = readJSON file: 'metrics.json'
+  def jsonBuilder = new groovy.json.JsonBuilder()
+  jsonBuilder.Sonar(
+  "Metrics" : resultJson
+  )
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/metrics.json")
+  file.write(jsonBuilder.toPrettyString())
+  return jsonBuilder
+
+ // create()
+}
